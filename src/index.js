917d599036106d625f1fcbc99b9f55abcf35f4d0
huangyhmax <184620541@qq.com>
2017-07-05 15:06:27 +0800

    修改引入jquery

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import _ from 'lodash';
-
+import $ from 'jquery';
 function component() {
-  var element = document.createElement('div');
+//   var element = document.createElement('div');
+  var element=$('<div></div>')
 
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.html(_.join(['this is test for ','imprt jquery haha test build'],''));
+//   return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
