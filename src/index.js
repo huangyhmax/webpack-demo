5113c8916b67d5c53a7fc0341fd3a3fbcc62cb4c
huangyh <184620541@qq.com>
2017-07-05 20:45:08 +0800

    修改打包语句为npm run build

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import _ from 'lodash';
+import j from 'jquery';
 
 function component() {
-  var element = document.createElement('div');
+  // var element = document.createElement('div');
+  var element = j('<div></div>');
 
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.html(_.join(['Hello','test jquery haha'], ' '))
 
-  return element;
+  // return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
