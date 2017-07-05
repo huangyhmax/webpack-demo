302da421b06ef73b3a1dec9557ebfdd4a2260687
huangyhmax <184620541@qq.com>
2017-07-05 15:25:32 +0800

    尝试引入foo.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import $ from 'jquery';
+import foo from './foo.js';
 function component() {
 //   var element = document.createElement('div');
   var element=$('<div></div>')
@@ -13,3 +14,5 @@ function component() {
 }
 
 document.body.appendChild(component());
+console.log(foo)
+console.log(foo())
